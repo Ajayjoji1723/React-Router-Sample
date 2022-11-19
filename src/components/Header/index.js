d3c7c93203e5a08a6caa8fec814e020afa14823d
Ajay Kumar Tuneti <ajayjoji1723@gmail.com>
2022-11-19 11:35:33 +0530

    react-router-sample

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom' 
+
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="blog-container">
+      <h1 className="blog-title">My Blog</h1>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/" className='link'>Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className='link'>About</Link>
+        </li>
+          <Link to="/contact" className='link'>Contact</Link>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
